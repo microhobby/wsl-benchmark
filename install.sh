e82b9866fab03551eecc7f43e30a5234be4ad0b0
Matheus Castello <matheus@castello.eng.br>
2021-04-25 10:59:11 -0300

    bootBenchmark: Make backup for wsl.conf
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 cp bootBenchmark /usr/bin
 chmod +x /usr/bin/bootBenchmark
 
+# make backup
+cp /etc/wsl.conf /etc/wsl.conf.back
+
 # create the boot section on wsl.conf
 cat << EOF >> /etc/wsl.conf
 
