9cf335e2c539bbcd08d1e9c496a4ee82d735e91f
Matheus Castello <matheus@castello.eng.br>
2021-04-25 11:23:50 -0300

    install: Firulas and style
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "📝 COPYING SCRIPTS"
 # copy files to the actual paths
 cp bootBenchmark /usr/bin
 chmod +x /usr/bin/bootBenchmark
 
+echo "🔙 BACKUP wsl.conf"
 # make backup
 cp /etc/wsl.conf /etc/wsl.conf.back
 
+echo "🔥 CREATING BOOT SECTION"
 # create the boot section on wsl.conf
 cat << EOF >> /etc/wsl.conf
 
