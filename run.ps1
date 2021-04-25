81a9f8752aae00ead367edf5fb6dacaf93bb653f
Matheus Castello <matheus@castello.eng.br>
2021-04-25 11:01:43 -0300

    run: Fix multi line script install
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- run.ps1
+++ run.ps1
@@ -1,13 +1,20 @@
 
+echo "⚙️ CONFIGURE"
 # configure
-wsl cd ~ && `
-    git clone https://github.com/microhobby/wsl-benchmark.git && `
-    cd wsl-benchmark && `
-    ./install.sh
+wsl bash -c 'cd /home && \
+git clone https://github.com/microhobby/wsl-benchmark.git && \
+cd wsl-benchmark && \
+sudo ./install.sh'
 
+echo "📴 TURNING OFF"
 # shutdown
 wsl --shutdown
 wsl --shutdown
 
+echo "🧪 TESTING"
 # runs
-wsl cat '/home/root/$(uname -r).log'
+wsl cat '/home/$(uname -r).log'
+
+echo "🧼 CLEANUP"
+# cleanup
+wsl bash -c 'sudo cp /etc/wsl.conf.back /etc/wsl.conf'
