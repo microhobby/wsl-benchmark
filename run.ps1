cc72dc48a1634b4e46123c3ab5b7b855a13987ad
Matheus Castello <matheus@castello.eng.br>
2021-04-25 11:47:28 -0300

    run: Add the argument to change the kernel to be loaded
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- run.ps1
+++ run.ps1
@@ -1,4 +1,14 @@
 
+# create kernel entry
+If ($args[0] -ne $null) {
+    echo "🔙 BACKUP .wslconfig"
+    cp ~\.wslconfig ~\.wslconfig.back
+    echo "🐱‍🐉 ADDING KERNEL ENTRY"
+    
+    $path = [regex]::escape($args[0])
+    echo "kernel=$path" >> ~\.wslconfig
+}
+
 echo "⚙️ CONFIGURE"
 # configure
 wsl bash -c 'cd /home && \
@@ -23,3 +33,4 @@ echo "--------------------------------------------------------------------------
 echo "🧼 CLEANUP"
 # cleanup
 wsl bash -c 'sudo cp /etc/wsl.conf.back /etc/wsl.conf && sudo rm -r /home/wsl-benchmark'
+cp ~\.wslconfig.back ~\.wslconfig
